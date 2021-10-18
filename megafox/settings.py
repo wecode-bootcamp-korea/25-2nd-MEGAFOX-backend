f2293901fb54328074b3e54d898077bdbf284de3
thisisempty <thisisemptyyy@gmail.com>
2021-10-18 17:18:50 +0900

    [MODIFY] import os environ variables

--- megafox/settings.py
+++ megafox/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib     import Path
-from my_settings import ALGORITHM, DATABASES, SECRET_KEY
-
+import os
+import my_settings
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,7 +76,16 @@ WSGI_APPLICATION = 'megafox.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = DATABASES
+DATABASES = {
+  'default' : {
+      'ENGINE': 'django.db.backends.mysql',
+      'NAME': os.environ['DB_NAME'],
+      'USER': os.environ['DB_USER'],
+      'PASSWORD': os.environ['DB_PASSWORD'],
+      'HOST': os.environ['DB_HOST'],
+      'PORT': os.environ['DB_PORT'],
+  }
+}
 
 
 # Password validation
@@ -122,4 +131,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALGORITHM = ALGORITHM
+ALGORITHM = os.environ["ALGORITHM"]
