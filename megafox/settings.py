37c00811a815248cac896caad0b78dbe13efa5f3
thisisempty <thisisemptyyy@gmail.com>
2021-10-20 18:56:04 +0900

    [ADD] cors

--- megafox/settings.py
+++ megafox/settings.py
@@ -142,3 +142,28 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALGORITHM = os.environ["ALGORITHM"]
+
+##CORS
+CORS_ORIGIN_ALLOW_ALL=True
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+)
+
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+)
