6cbb9f6839c1015a35788e511079392f20b86692
thisisempty <thisisemptyyy@gmail.com>
2021-10-22 15:50:53 +0900

    [CREATE] ReviwView, unit test [ADD] review app path, view path

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
 
-urlpatterns = []
+from reviews.views import *
+
+urlpatterns = [
+    path('/comment', ReviewView.as_view()),
+    path('/comment/<int:review_id>', ReviewView.as_view())
+]
